9c4aa942b79b2bee9930108b4fc87cb0c1fe1c46
briody <telebriody@hotmail.com>
2013-03-02 14:36:56 -0700

    Silver Challenge

--- HeavyRotation/HeavyRotation/HeavyViewController.h
+++ HeavyRotation/HeavyRotation/HeavyViewController.h
@@ -7,7 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
-
+//Silver Challenge
 @interface HeavyViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIView *heavyView;
+@property (weak, nonatomic) IBOutlet UIButton *leftButton;
+@property (weak, nonatomic) IBOutlet UIButton *rightButton;
+@property (weak, nonatomic) IBOutlet UISlider *topSlider;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
--- HeavyRotation/HeavyRotation/HeavyViewController.m
+++ HeavyRotation/HeavyRotation/HeavyViewController.m
@@ -10,6 +10,33 @@
 
 @implementation HeavyViewController
 
+
+//Set the autoresizing mask programmatically
+//Views aren't created when controller is initialized
+//So we want to do this in ViewDidLoad not in init
+
+//Silver Challenge
+- (void)viewDidLoad
+{
+    //The Struts actually 
+    //Image view
+    [_imageView setAutoresizingMask:UIViewAutoresizingFlexibleHeight
+     | UIViewAutoresizingFlexibleWidth];
+    
+    //Slider
+    [_topSlider setAutoresizingMask:
+     UIViewAutoresizingFlexibleBottomMargin
+     | UIViewAutoresizingFlexibleWidth];
+    
+    //Left Button
+    [_leftButton setAutoresizingMask:UIViewAutoresizingFlexibleRightMargin
+     | UIViewAutoresizingFlexibleTopMargin];
+    
+    //Right Button
+    [_rightButton setAutoresizingMask:UIViewAutoresizingFlexibleLeftMargin
+     | UIViewAutoresizingFlexibleTopMargin];
+}
+
 //Deprecated
 -(BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)x
 {
@@ -18,5 +45,8 @@
     return (x == UIInterfaceOrientationPortrait
             || UIInterfaceOrientationIsLandscape(x));
 }
+
+
+
 
 @end
